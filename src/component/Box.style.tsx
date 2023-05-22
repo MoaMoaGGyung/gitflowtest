8fae6b422a056be8ae7af56bb1b4d83d5c03e827
root <root@DESKTOP-LS711CJ.localdomain>
2023-05-22 22:14:57 +0900

    redbox create

---
+++ src/component/Box.style.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const RedBox = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 100px;
+`;
---
+++ src/component/Box.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { RedBox } from "./Box.style";
+
+export default function Box() {
+  return (
+    <RedBox></RedBox>
+  )
+}
